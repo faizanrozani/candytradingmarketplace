b33dafe0c09022c60f2b58c83e8c9c9f068b1171
ekrzysz <EKRZYSZ1@depaul.edu>
2022-11-05 17:59:33 -0500

    nav placement

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -14,32 +14,30 @@ export default function Navbar() {
         <div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <Logo />
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Brand</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                        </ul>
-                        <ul class="navbar-nav">
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Menu </a>
-                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                <div class="container">
+                <div class="row">
+    <div class="col-6">
+      
+    </div>
+    <div class="col-3">
+    
+    </div>
+    <div class="col-3">
+     
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-6">
+      button one
+    </div>
+    <div class="col-3">
+      button two
+    </div>
+  </div>
+  
+</div>
             </nav>
+            
         </div>
     );
 }
