be82c1d23a62e7ffb4a2aeeac36083e2222fddde
ekrzysz <EKRZYSZ1@depaul.edu>
2022-11-06 02:39:45 -0600

    finished front end

--- client/src/components/Overview.js
+++ client/src/components/Overview.js
@@ -4,9 +4,9 @@ import Content from '../Content';
 
 export default function Overview() {
     return (
-        <div class="overview bg-light">
-            
-            <h3 class="text-center">Candy Trading Marketplace Overview</h3>
+        <div class="profile bg-light">
+            <div class="inner-profile-modified">
+            <h3 class="text-center title-style">Who are We?</h3>
 
             <div class="container">
                 <h6 class="text-center">Did your child go trick or treating? Do they have candy they don't want and are they throwing a fit
@@ -14,6 +14,7 @@ export default function Overview() {
                 your child has, and what candies they prefer. In the view trades tab, you are able to see other users candy inventory and their prefrences.
                 From there, you can click to make a trade with them, if you would like. 
                 But before any of this, you have to Login/Sign Up. </h6>
+            </div>
             </div>
         </div>
         
--- client/src/components/SetProfile.js
+++ client/src/components/SetProfile.js
@@ -2,15 +2,34 @@ import React from 'react';
 
 export default function SetProfile() {
     return (
-        <div class="profile bg-light text-center">
-          <h1>Set Child's Profile</h1>
-          <p>Quantity</p>
-          <select>
-            <option></option>
-          </select>
-          <input id="userInputs" type="number" placeholder="Text"></input>
-          <input id="userInput" type="text" placeholder="Text"></input>
-          <button onclick="returnText()" class="btn btn-warning">Submit</button>
+        <div class="profile bg-light">
+          <div class="inner-profile">
+          <h1 class="title-style">Set Candy Quantities</h1>
+          <form>
+            <div class="form-Lollipops">
+            <label class="form-Lollipops"for="userInput-Lollipops">
+              Number of Lollipops: 
+            </label>
+            <input id="userInput-Lollipops" type="number"></input>
+            </div>
+            <br></br>
+            <div class="form-Choclate">
+            <label for="userInput-Chocalate">
+              Number of Chocalates: 
+            </label>
+            <input id="userInput-Choclate" type="number"></input>
+            </div>
+            <br></br>
+            <div class="form-GumDrops">
+            <label class="form-GumDrops" for="userInput-GunDrops">
+              Number of Gum Drops: 
+            </label>
+            <input id="userInput-GumDrops" type="number"></input>
+            </div>
+            <br></br>
+            <button onclick="returnText()" class="btn btn-warning">Submit</button>
+          </form>
+          </div>
           <script>
           let input = document.getElementById("userInput").value;
           alert(input) </script>
